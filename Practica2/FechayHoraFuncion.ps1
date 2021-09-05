49c6691bc08aae3323bd8475ef711b892608047a
Your Name <you@example.com>
2021-09-04 22:15:19 -0500

    Aquí el script de PowerShell

---
+++ Practica2/FechayHoraFuncion.ps1
@@ -0,0 +1,20 @@
+﻿function Fecha{
+for ($i=1){
+Get-Date -DisplayHint Date
+exit
+}
+}
+function Hora{
+for ($i=2){
+Get-Date -DisplayHint Time
+exit
+}
+}
+
+Write-Host "¡Hola! Bienvenido a mi función de PowerShell"
+$i= Read-Host -Prompt "¿Qué desea ver? [1] Fecha, [2] Hora"
+switch($i){
+    1{
+    Fecha}2{
+    Hora}
+    }
