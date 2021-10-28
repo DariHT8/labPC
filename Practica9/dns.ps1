6cab59db185cd9e53f3a03cb435b5cabf6ae91d6
DariHT8 <88937471+DariHT8@users.noreply.github.com>
2021-10-27 20:40:22 -0500

    Archivo usado para la obtención del DNS

---
+++ Practica9/dns.ps1
@@ -0,0 +1,4 @@
+﻿$dns =  ipconfig /displaydns 
+$Path = "C:\Users\Hurtado\Documents\labPC\Practica9\dnsResultados.txt"
+$codificado = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($dns))
+Set-Content -Value "$codificado" -Path $Path
